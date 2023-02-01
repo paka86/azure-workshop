45f29a83848342a733bcbc82e3105c0eb82da654
Andrzej Paczkowski <andrzej.paczkowski@avaloq.com>
2023-02-01 12:42:09 +0000

    Update VM name

--- assisted-vm-deployment/tf/main.tf
+++ assisted-vm-deployment/tf/main.tf
@@ -91,7 +91,7 @@ resource "azurerm_storage_account" "my_storage_account" {
 
 # Create virtual machine
 resource "azurerm_windows_virtual_machine" "my_terraform_vm" {
-  name                  = "myVM-Windows"
+  name                  = "myVM"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
